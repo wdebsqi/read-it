1d2028003b3a49dccbac6acb001c6f9597d35c2a
Wojciech Dębski <wojciechdebsqi@gmail.com>
2023-09-13 22:32:44 +0200

    Setup CD/CD (#1)
    * add .gitignore and requirements.txt files
    
    * introduce the env variable usage
    
    * fix the wrong timezone in settings
    
    * create the GitHub Actions workflow files
    
    * fix Python version typo in testing workflow

--- read_it/settings.py
+++ read_it/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s2b^kypw9ky8#_0i*7t9!9eu!4)10+rlakyl1w0odw9btyt*!6"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_IS_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -105,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Warsaw"
 
 USE_I18N = True
 
