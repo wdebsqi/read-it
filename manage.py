359d8c1dd3e625a12542c2044e9ea305a4ea392f
Wojtek <wojciechdebsqi@gmail.com>
2023-09-13 22:25:53 +0200

    introduce the env variable usage

--- manage.py
+++ manage.py
@@ -3,9 +3,13 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 
 def main():
     """Run administrative tasks."""
+    load_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "read_it.settings")
     try:
         from django.core.management import execute_from_command_line
