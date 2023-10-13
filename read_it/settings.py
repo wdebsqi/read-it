edb3d5d1ab73b60516e75c3228e19efb0f585338
Wojtek <wojciechdebsqi@gmail.com>
2023-10-13 23:57:04 +0200

    add new app to dependencies; create migration with log table

--- read_it/settings.py
+++ read_it/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "db_logging.apps.DbLoggingConfig",
     "read_it.apps.ReadItConfig",
 ]
 
