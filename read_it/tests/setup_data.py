713339756ec2292eca0a28b60c3fce48e85d61c5
Wojtek <wojciechdebsqi@gmail.com>
2024-01-05 19:28:24 +0100

    fix description in the test Books

--- read_it/tests/setup_data.py
+++ read_it/tests/setup_data.py
@@ -5,6 +5,6 @@ def create_new_book(id: int | None = None) -> Book:
     return Book.objects.create(
         id=id,
         title=f"This is a test book number {id}",
-        description="This is a test description number",
+        description=f"This is a test description number {id}",
         published_on="2021-01-01",
     )
